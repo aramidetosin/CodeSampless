6ea4c27e453dc26b6d0ced0dc966c7ea5a267f57
Aramide Oluwatosin <aoluwatosin10@gmail.com>
2020-12-06 22:15:37 +0000

    update with mc-chassis

--- multiareaOSPF/rpc_test.py
+++ multiareaOSPF/rpc_test.py
@@ -18,24 +18,30 @@ nr = InitNornir(config_file=f"{script_dir}/config.yml")
 # xpath = 'interfaces/interface'
 # xml = '<interfaces></interfaces>'
 # database = 'committed'
-# extras = {
-#     "level-extra": "terse",
-#     "interface-name": "ge-0/0/0"
-# }
-
 extras = {
-    "level-extra": "brief",
-    "neighbor": "192.168.34.3"
+    "level-extra": "terse",
+    "interface-name": "fxp0"
 }
 
-response = nr.run(
-    task=pyez_rpc, func='get-ospf-neighbor-information', extras=None
-)
+# extras = {
+    # "level-extra": "brief",
+    # "neighbor": "192.168.34.3"
+# }
+
 # response = nr.run(
-#     task=pyez_rpc, func='get-interface-information', extras=extras)
+    # task=pyez_rpc, func='get-ospf-neighbor-information', extras=None
+# )
+response = nr.run(
+    task=pyez_rpc, func='get-interface-information', extras=extras)
 
 # response is an AggregatedResult, which behaves like a list
 # there is a response object for each device in inventory
 devices = []
+# print_result(response)
 for dev in response:
-    print(response[dev].result)
+    print(dev)
+    print(response[dev].result['interface-information']['physical-interface']['logical-interface'])
+    # print(dev)
+#     # for i in response[dev].result['ospf-neighbor-information']['ospf-neighbor']:
+#     #     print(i['neighbor-id'])
+    # print(f"{dev}: Interface ge-0/0/0 is oper-status [red]{response[dev].result['interface-information']['physical-interface']['logical-interface']['oper-status']}[/red]")
